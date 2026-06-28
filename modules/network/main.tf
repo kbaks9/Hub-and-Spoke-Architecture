123611cebfcf60c03fa5b546e2127f342e76b6f8
kbaks9 <kamaal.bakar@outlook.com>
2026-06-28 22:45:18 +0100

    Full implemention of vnets and subnets

---
+++ modules/network/main.tf
@@ -0,0 +1,75 @@
+# Hub VNet & Subnet
+resource "azurerm_virtual_network" "hub_vnet" {
+  name                = var.vnet_hub_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.hub_address_space
+  tags                = var.hub_tags
+}
+
+resource "azurerm_subnet" "hub_subnet" {
+  name                 = var.hub_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = var.hub_address_prefixes
+}
+
+# Spoke 1 VNet & Subnet
+resource "azurerm_virtual_network" "spoke1_vnet" {
+  name                = var.spoke1_vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.spoke1_address_space
+  tags                = var.spoke1_tags
+}
+
+resource "azurerm_subnet" "spoke1_subnet" {
+  name                 = var.spoke1_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.spoke1_vnet.name
+  address_prefixes     = var.spoke1_address_prefixes
+}
+
+# Spoke 2 VNet & Subnet
+resource "azurerm_virtual_network" "spoke2_vnet" {
+  name                = var.spoke2_vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.spoke2_address_space
+  tags                = var.spoke2_tags
+}
+
+resource "azurerm_subnet" "spoke2_subnet" {
+  name                 = var.spoke2_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.spoke2_vnet.name
+  address_prefixes     = var.spoke2_address_prefixes
+}
+
+resource "azurerm_subnet" "firewall_subnet" {
+  name                 = var.firewall_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = var.firewall_address_prefixes
+}
+
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = var.bastion_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = var.bastion_address_prefixes
+}
+
+resource "azurerm_subnet" "gateway_subnet" {
+  name                 = var.gateway_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = var.gateway_address_prefixes
+}
+
+resource "azurerm_subnet" "monitor_subnet" {
+  name                 = var.monitor_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = var.monitor_address_prefixes
+}
