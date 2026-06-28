1d911009c03369c1c2d05e3f8be83e8c177b317e
kbaks9 <kamaal.bakar@outlook.com>
2026-06-28 22:46:49 +0100

    Reference variables from modules added

---
+++ variable.tf
@@ -0,0 +1,101 @@
+# Global configs
+variable "resource_group" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+# Networking
+variable "vnet_hub_name" {
+  type = string
+}
+
+variable "spoke1_vnet_name" {
+  type = string
+}
+
+variable "spoke2_vnet_name" {
+  type = string
+}
+
+variable "hub_subnet_name" {
+  type = string
+}
+
+variable "spoke1_subnet_name" {
+  type = string
+}
+
+variable "spoke2_subnet_name" {
+  type = string
+}
+
+variable "hub_address_space" {
+  type = list(string)
+}
+
+variable "spoke1_address_space" {
+  type = list(string)
+}
+
+variable "spoke2_address_space" {
+  type = list(string)
+}
+
+variable "hub_address_prefixes" {
+  type = list(string)
+}
+
+variable "spoke1_address_prefixes" {
+  type = list(string)
+}
+
+variable "spoke2_address_prefixes" {
+  type = list(string)
+}
+
+variable "gateway_address_prefixes" {
+  type = list(string)
+}
+
+variable "bastion_address_prefixes" {
+  type = list(string)
+}
+
+variable "firewall_address_prefixes" {
+  type = list(string)
+}
+
+variable "monitor_address_prefixes" {
+  type = list(string)
+}
+
+variable "hub_tags" {
+  type = map(string)
+}
+
+variable "spoke1_tags" {
+  type = map(string)
+}
+
+variable "spoke2_tags" {
+  type = map(string)
+}
+
+variable "gateway_subnet_name" {
+  type = string
+}
+
+variable "bastion_subnet_name" {
+  type = string
+}
+
+variable "firewall_subnet_name" {
+  type = string
+}
+
+variable "monitor_subnet_name" {
+  type = string
+}
