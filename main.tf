b941b84e6018e4fc4b77511bf463b3a4b94ffa19
kbaks9 <kamaal.bakar@outlook.com>
2026-07-01 20:32:16 +0100

    Added, updated, and rearranged modules within main

--- main.tf
+++ main.tf
@@ -12,24 +12,22 @@ module "network" {
   spoke1_vnet_name = var.spoke1_vnet_name
   spoke2_vnet_name = var.spoke2_vnet_name
 
-  spoke1_subnet_name   = var.spoke1_subnet_name
-  spoke2_subnet_name   = var.spoke2_subnet_name
-  gateway_subnet_name  = var.gateway_subnet_name
-  bastion_subnet_name  = var.bastion_subnet_name
-  firewall_subnet_name = var.firewall_subnet_name
-  monitor_subnet_name  = var.monitor_subnet_name
+  spoke1_subnet_name              = var.spoke1_subnet_name
+  spoke2_subnet_name              = var.spoke2_subnet_name
+  application_gateway_subnet_name = var.application_gateway_subnet_name
+  bastion_subnet_name             = var.bastion_subnet_name
+  firewall_subnet_name            = var.firewall_subnet_name
 
   hub_tags    = var.hub_tags
   spoke1_tags = var.spoke1_tags
   spoke2_tags = var.spoke2_tags
 
-  hub_address_prefixes      = var.hub_address_prefixes
-  spoke1_address_prefixes   = var.spoke1_address_prefixes
-  spoke2_address_prefixes   = var.spoke2_address_prefixes
-  gateway_address_prefixes  = var.gateway_address_prefixes
-  bastion_address_prefixes  = var.bastion_address_prefixes
-  firewall_address_prefixes = var.firewall_address_prefixes
-  monitor_address_prefixes  = var.monitor_address_prefixes
+  hub_address_prefixes                 = var.hub_address_prefixes
+  spoke1_address_prefixes              = var.spoke1_address_prefixes
+  spoke2_address_prefixes              = var.spoke2_address_prefixes
+  application_gateway_address_prefixes = var.application_gateway_address_prefixes
+  bastion_address_prefixes             = var.bastion_address_prefixes
+  firewall_address_prefixes            = var.firewall_address_prefixes
 
   hub_address_space    = var.hub_address_space
   spoke1_address_space = var.spoke1_address_space
@@ -41,12 +39,11 @@ module "nsg" {
   resource_group_name = azurerm_resource_group.rg-grp.name
   location            = azurerm_resource_group.rg-grp.location
 
-  nsg_spoke1_name   = var.nsg_spoke1_name
-  nsg_spoke2_name   = var.nsg_spoke2_name
-  nsg_gateway_name  = var.nsg_gateway_name
-  nsg_bastion_name  = var.nsg_bastion_name
-  nsg_firewall_name = var.nsg_firewall_name
-  nsg_monitor_name  = var.nsg_monitor_name
+  nsg_spoke1_name = var.nsg_spoke1_name
+  nsg_spoke2_name = var.nsg_spoke2_name
+
+  subnet_spoke1_id = module.network.subnet_spoke1_id
+  subnet_spoke2_id = module.network.subnet_spoke2_id
 
   hub_tags    = var.hub_tags
   spoke1_tags = var.spoke1_tags
