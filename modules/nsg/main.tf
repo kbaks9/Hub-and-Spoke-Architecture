f6733b5a524aa84b0e44a5ca282a0f1308a007d5
kbaks9 <kamaal.bakar@outlook.com>
2026-07-01 20:31:03 +0100

    Added security rules for both spoke1&2 vnets

--- modules/nsg/main.tf
+++ modules/nsg/main.tf
@@ -12,27 +12,107 @@ resource "azurerm_network_security_group" "nsg_spoke2" {
   tags                = var.spoke2_tags
 }
 
-resource "azurerm_network_security_group" "nsg_monitor" {
-  name                = var.nsg_monitor_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+resource "azurerm_subnet_network_security_group_association" "spoke1" {
+  subnet_id                 = var.subnet_spoke1_id
+  network_security_group_id = azurerm_network_security_group.nsg_spoke1.id
 }
 
-# Next to add is this:
-# resource "azurerm_subnet_network_security_group_association" "spoke1" {
-#   subnet_id                 = azurerm_subnet.spoke1_subnet.id
-#   network_security_group_id = azurerm_network_security_group.nsg_spoke1.id
-# }
+resource "azurerm_subnet_network_security_group_association" "spoke2" {
+  subnet_id                 = var.subnet_spoke2_id
+  network_security_group_id = azurerm_network_security_group.nsg_spoke2.id
+}
 
-# resource "azurerm_subnet_network_security_group_association" "spoke2" {
-#   subnet_id                 = azurerm_subnet.spoke2_subnet.id
-#   network_security_group_id = azurerm_network_security_group.nsg_spoke2.id
-# }
+### Allow ###
+## Inbound ##
+# Spoke1 allows HTTPS from Application Gateway
+resource "azurerm_network_security_rule" "allow_inbound_appgw_https_spoke1" {
+  name                        = "allow-https-from-appgw-spoke1"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "10.0.2.0/24" # App Gateway subnet
+  destination_address_prefix  = "*"           # Spoke1 subnet (implicit)
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg_spoke1.name
+}
 
-# resource "azurerm_subnet_network_security_group_association" "monitor" {
-#   subnet_id                 = azurerm_subnet.monitor_subnet.id
-#   network_security_group_id = azurerm_network_security_group.nsg_monitor.id
-# }
+# Spoke1 allows SSH from Bastion
+resource "azurerm_network_security_rule" "allow_bastion_ssh_spoke1" {
+  name                        = "allow-ssh-from-bastion-spoke1"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "10.0.3.0/26" # Bastion subnet
+  destination_address_prefix  = "*"           # Spoke1 subnet (implicit)
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg_spoke1.name
+}
+
+## Deny ##
+resource "azurerm_network_security_rule" "deny_all_outbound_traffic_spoke1" {
+  name                        = "deny-all-outbound-traffic"
+  priority                    = 500
+  direction                   = "Outbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg_spoke1.name
+}
+
+### Allow ###
+## Inbound ##
+# Spoke2 allows App Gateway connection
+resource "azurerm_network_security_rule" "allow_appgw_https_spoke2" {
+  name                        = "allow-https-from-appgw-spoke2"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "10.0.2.0/24" # App Gateway subnet
+  destination_address_prefix  = "*"           # Spoke2 subnet (implicit)
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg_spoke2.name
+}
+
+# Spoke2 allows Bastion connection
+resource "azurerm_network_security_rule" "allow_bastion_ssh_spoke2" {
+  name                        = "allow-ssh-from-bastion-spoke2"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "10.0.3.0/26" # Bastion subnet
+  destination_address_prefix  = "*"           # Spoke2 subnet (implicit)
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg_spoke2.name
+}
 
 
-
+## Deny ##
+resource "azurerm_network_security_rule" "deny_all_outbound_traffic_spoke2" {
+  name                        = "deny-all-outbound-traffic"
+  priority                    = 500
+  direction                   = "Outbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg_spoke2.name
+}
