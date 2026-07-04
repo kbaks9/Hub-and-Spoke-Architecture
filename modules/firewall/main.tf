1850baf3f6ed18d4332d8c728731afe85244fce0
kbaks9 <kamaal.bakar@outlook.com>
2026-07-05 00:09:05 +0100

    Added firewall resource

---
+++ modules/firewall/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_firewall" "firewall" {
+  name                = var.firewall_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = var.firewall_subnet_id
+    public_ip_address_id = var.firewall_public_ip_address_id
+  }
+}
