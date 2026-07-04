0ee095dab38e8185ab0e0a0f1c2411b881e83cee
kbaks9 <kamaal.bakar@outlook.com>
2026-07-05 00:09:19 +0100

    Variables for firewall resources

---
+++ modules/firewall/variable.tf
@@ -0,0 +1,20 @@
+# Global configs
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "firewall_name" {
+  type = string
+}
+
+variable "firewall_subnet_id" {
+  type = string
+}
+
+variable "firewall_public_ip_address_id" {
+  type = string
+}
