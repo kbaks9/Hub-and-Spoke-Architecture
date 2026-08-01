45a7c5d82ddd52afeca05bbffae67946538bb3bb
kbaks9 <kamaal.bakar@outlook.com>
2026-08-01 13:30:10 +0100

    Added output value for firewall resource id

--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -1,3 +1,7 @@
 output "firewall_private_ip_address" {
   value = azurerm_firewall.firewall.ip_configuration[0].private_ip_address
 }
+
+output "firewall_target_id" {
+  value = azurerm_firewall.firewall.id
+}
