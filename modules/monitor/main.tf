c06a736836e2daeb31bd2175ad2cb164480c82d0
kbaks9 <kamaal.bakar@outlook.com>
2026-08-01 17:12:45 +0100

    Added action group & alert

--- modules/monitor/main.tf
+++ modules/monitor/main.tf
@@ -34,3 +34,62 @@ resource "azurerm_monitor_diagnostic_setting" "firewall" {
     category = "AllMetrics"
   }
 }
+
+resource "azurerm_monitor_action_group" "ag_vmss" {
+  name                = "ag-vmss-alerts"
+  resource_group_name = var.resource_group_name
+  short_name          = "vmss-platform"
+
+  email_receiver {
+    name          = "vmss-alerts"
+    email_address = "realbaks33@gmail.com"
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "vmss_cpu" {
+  name                = "alert-vmss-high-cpu"
+  resource_group_name = var.resource_group_name
+  scopes              = [var.vmss_target_id]
+
+  description = "VMSS CPU usage above 80%"
+  severity    = 2
+
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachineScaleSets"
+    metric_name      = "Percentage CPU"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 80
+  }
+
+  window_size = "PT5M"
+  frequency   = "PT1M"
+
+  action {
+    action_group_id = azurerm_monitor_action_group.ag_vmss.id
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "vmss_availability" {
+  name                = "alert-vmss-availability"
+  resource_group_name = azurerm_resource_group.rg.name
+  scopes              = [var.vmss_target_id]
+
+  description = "VMSS availability issue"
+  severity    = 1
+
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachineScaleSets"
+    metric_name      = "Available VM Instances"
+    aggregation      = "Average"
+    operator         = "LessThan"
+    threshold        = 2
+  }
+
+  window_size = "PT5M"
+  frequency   = "PT1M"
+
+  action {
+    action_group_id = azurerm_monitor_action_group.ag_vmss.id
+  }
+}
